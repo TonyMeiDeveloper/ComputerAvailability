17b83879b904e8a27aba574ce31bdabb06b251df
TonyMeiDeveloper <tony.mei.marquette@gmail.com>
2016-04-22 14:26:12 -0500

    Create computer_availability.sql

---
+++ computer_availability.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE 'computer_availability'
+CREATE TABLE `compstatus` (
+  `computer_name` varchar(250) NOT NULL default "",
+  `status` int(11) default NULL,
+  `computer_type` varchar(250) default NULL,
+  `left_pos` int(11) default NULL,
+  `top_pos` int(11) default NULL,
+  `updated_at` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`computer_name`)
+)
