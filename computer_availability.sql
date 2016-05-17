eddcc50d81165a44a7cfe95aa62dd1466a347918
TonyMeiDeveloper <tony.mei.marquette@gmail.com>
2016-05-17 14:32:49 -0500

    Update computer_availability.sql

--- computer_availability.sql
+++ computer_availability.sql
@@ -5,6 +5,7 @@ CREATE TABLE computer_availability.compstatus (
   computer_name varchar(250) NOT NULL default "",
   status int(11) default NULL,
   computer_type varchar(250) default NULL,
+  configuration varchar(250) default NULL,
   floor varchar(250) default NULL,
   left_pos int(11) default NULL,
   top_pos int(11) default NULL,
