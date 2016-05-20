85864762a8afe994485c073119254254e30e0608
TonyMeiDeveloper <tony.mei.marquette@gmail.com>
2016-05-20 15:57:50 -0500

    Add files via upload

--- logout.pl
+++ logout.pl
@@ -1,35 +1,67 @@
 #!/usr/bin/perl -w
 
+
 use LWP;
 use Sys::Hostname;
 use IO::Socket;
-my $host = hostname();
+#my $host = hostname();
  #the hostname after which you can find out the IP address
-my $ipaddr = inet_ntoa(scalar gethostbyname($host || 'localhost'));
+#my $ipaddr = inet_ntoa(scalar gethostbyname($host || 'localhost'));
 
+
+#Get Computer Name
+my $ComputerName= qx(networksetup -getcomputername);
+#Parse out what you need
+@ComputerNameArray=split(' ', $ComputerName);
+#print $ComputerName;
+#print $ComputerNameArray[4];
+
+
+#Determine Floor based on the computername
+if(index($ComputerName,"2-0")!=-1)
+{
+	$floor="RaynorSecond";
+}
+else
+{
+	$floor="RaynorLower";
+}
+
+
+#Determine positions of the pod that the Mac belongs to
+if(index($ComputerName,"2-0")!=-1)
+{
+	$left_pos="50";
+	$top_pos="11";
+}
+elsif(index($ComputerName,"-A")!=-1)
+{
+	$left_pos="44";
+	$top_pos="21";
+}
+else #Macs in Pod B
+{
+	$left_pos="54";
+	$top_pos="21";
+}
+
+
+
+
+
+#HTTP Post Request
 $ua = LWP::UserAgent->new;
 #timeout in 60 seconds if we can't make a connection
 $ua->timeout(60);
-
 #send the HTTP request with the status of 0 and computer name and the host
-$request = $ua->post('http://yourdomain.edu/statuschange.php',
+$ua->post('enterdomain/statuschange.php',
             	["status" => "0",
-            	"workstation" => $ipaddr,
-   	 "host" => $host]);
-
-print $host;
-print "\n";
-print $ipaddr; 
-print "\n";
-#if successful log 200 status else quit and log error
-if ($request->is_success) {
-	print "Successfully connected. Receiving info from PHP script:\n";
-	$content = $request->content;
-    #print "Content-type: text/html\\n\\n";
-    print $content;
-} else {
-      die "Can't get to URL", $request->status_line;
-      print "fail";
-}
+            	"workstation" => 'Mac '. $ComputerNameArray[$#ComputerNameArray],
+   	 			"computertype" => "Mac",
+   	 			"configuration" => "Mac",
+   	 			"floor" => $floor,
+   	 			"left_pos" => $left_pos,
+   	 			"top_pos" => $top_pos
+   	 			]);
 
 exit;
